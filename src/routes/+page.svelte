bcfac9fca98b30974065c66ea7a40b616502b4cf
FuseTim <fusetim@gmx.com>
2024-10-05 16:21:44 +0200

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<svelte:options runes={true} />
+<script lang="ts">
+	import Cagnote from '$lib/cagnote/cagnote.svelte';
+
+	let value = $state(0n);
+
+    function add(incr: bigint) {
+        value = value + incr
+    }
+</script>
+
+<main
+	class="border-8 border-yellow-500 w-screen h-screen overflow-clip flex flex-col justify-center items-center"
+>
+	<Cagnote value={value}></Cagnote>
+	<div class="mt-8 p-4 border-4 border-yellow-500 rounded-xl flex flex-row gap-1">
+        {#each [100000n,10000n,1000n,100n,10n,1n] as incr}
+		<button
+            onclick={() => add(incr)}
+			class="font-bold bg-yellow-500 hover:bg-yellow-600 active:bg-orange-500 px-2 py-1 border-yellow-700 border-2 transition-colors"
+			>+{Number(incr)/100}€</button
+		>
+        {/each}
+	</div>
+</main>
