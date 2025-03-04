232f5b79ce376cca57baa0b81bfb525dbe423d22
FuseTim <fusetim.log@gmx.com>
2025-03-04 18:44:03 +0100

    ci: add github pages deployment

--- src/app.tsx
+++ src/app.tsx
@@ -6,6 +6,7 @@ import "./app.css";
 export default function App() {
   return (
     <Router
+      base={import.meta.env.SERVER_BASE_URL}
       root={(props) => <Suspense>{props.children}</Suspense>}>
       <FileRoutes />
     </Router>
